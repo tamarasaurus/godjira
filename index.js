947ff5e17ff6f1de46532024d887e953e2312692
Tamara Chahine <chahine.tamara@abc.net.au>
2014-07-23 23:08:38 +1000

    Install express and set up project route

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+
 var config = require('./config');
+var Project = require('./parser/project');
+var project = new Project();
 var Jira = require('jira-tamarasaurus').JiraApi;
-var jira = new Jira('https', config.host, config.port, config.user, config.password, '2.0.alpha1');
+var jira = new Jira('https', config.host, config.port, config.user, config.password, '2');
 
 
-console.log(jira);
+
+// Set the project to be used
+
+app.get('/project/:key', function(req, res) {
+	jira.getProject(req.params.key, function(e, response, body) {
+		console.log(project.set(response));
+		res.json(project.set(response));
+	});
+});
+
+var server = app.listen(3000, function() {
+    console.log('Listening on port %d', server.address().port);
+});
---
+++ parser/project.js
@@ -0,0 +1,32 @@
+module.exports = (function(){
+
+    var project = function(){
+
+        this.item = {};
+
+        this.set = function(p){
+            this.item = p;
+            return p;
+        };
+
+        this.getKey = function(){
+
+        };
+
+        this.getName = function(){
+
+        };
+
+        this.getLead = function(){
+
+        };
+
+        this.getVersions = function(){
+
+        }
+
+    };
+
+
+    return project;
+}());
