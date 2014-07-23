b93e0e53d5544e9b68852b703a44ef6d6fb72b37
Tamara Chahine <chahine.tamara@abc.net.au>
2014-07-23 23:17:02 +1000

    Add initial project view and change dir for parsers

--- index.js
+++ index.js
@@ -4,19 +4,22 @@ var express = require('express');
 var app = express();
 
 var config = require('./config');
-var Project = require('./parser/project');
+var Project = require('./lib/parser/project');
 var project = new Project();
 var Jira = require('jira-tamarasaurus').JiraApi;
 var jira = new Jira('https', config.host, config.port, config.user, config.password, '2');
 
 
+app.set('views', __dirname + '/views/');
+app.set('view engine', 'jade');
 
 // Set the project to be used
 
 app.get('/project/:key', function(req, res) {
 	jira.getProject(req.params.key, function(e, response, body) {
 		console.log(project.set(response));
-		res.json(project.set(response));
+		res.render('project', {'project': JSON.stringify(project.set(response))});
+		// res.json();
 	});
 });
 
