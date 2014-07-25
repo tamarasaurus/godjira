b597a3ffd6acb87865018c71aace26c711b1da54
Tamara Chahine <chahine.tamara@abc.net.au>
2014-07-25 14:46:37 +1000

    Update rapidview urls and added sprint parser

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
 'use strict';
 
-var _ = require('underscore');
-var config = require('./config');
-
-var express = require('express');
-var app = express();
+// Dependencies
+var _ = require('underscore'),
+config = require('./config'),
+express = require('express'),
+app = express();
 
 // Parsers
-var Project = require('./lib/parser/project');
-var User = require('./lib/parser/user');
-var Rapid = require('./lib/parser/rapid');
-
-var Jira = require('jira-tamarasaurus').JiraApi;
-var jira = new Jira('https', config.host, config.port, config.user, config.password, '2');
-
-
-var user = new User();
-var project = new Project();
-var rapid = new Rapid();
+var Project = require('./lib/parser/project'),
+User = require('./lib/parser/user'),
+Rapid = require('./lib/parser/rapid'),
+Sprint = require('./lib/parser/sprint');
+var user = new User(),
+project = new Project(),
+rapid = new Rapid(),
+sprint = new Rapid();
 
 
+// Jira api
+var Jira = require('jira-tamarasaurus').JiraApi,
+jira = new Jira('https', config.host, config.port, config.user, config.password, '2');
+
+// App settings
 app.set('views', __dirname + '/views/');
 app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/public'));
@@ -64,15 +66,30 @@ app.get('/people/:nickname', function(req, res) {
 	});
 });
 
-app.get('/activeboard', function(req, res) {
-	jira.findRapidView(function(response){
+app.get('/board', function(req, res) {
+	jira.findRapidView(function(response) {
 		var view = rapid.getActive(rapid.getRapidsFromProject(response, config.project));
 		res.json(view);
 	});
 });
 
+app.get('/sprint/latest', function(req, res) {
+
+	jira.findRapidView(function(response) {
+		var view = rapid.getActive(rapid.getRapidsFromProject(response, config.project));
+
+		console.log(view.id);
+
+		jira.getSprint(view.id, function(e, r, body) {
+			// getactive
+			res.json(r);
+		});
+	});
+
+});
+
 app.get('/sprint/:rapidviewid', function(req, res) {
-	jira.getSprint(req.params.rapidviewid, function(e, response){
+	jira.getSprint(req.params.rapidviewid, function(e, response) {
 		res.json(response);
 	});
 });
@@ -81,9 +98,9 @@ app.get('/sprint/:rapidviewid', function(req, res) {
 // https://abcdevelop.jira.com/rest/greenhopper/1.0/rapidviewconfig/editmodel.json?rapidViewId=121
 //  rest/greenhopper/1.0/xboard/work/allData/?rapidViewId=121
 
-		// jira.getLastSprintForRapidView(req.params.id, function(e, response, body) {
-	// 	res.json(response);
-	// });
+// jira.getLastSprintForRapidView(req.params.id, function(e, response, body) {
+// 	res.json(response);
+// });
 // });
 // app.get('/people/:nickname/issues', function(req, res) {
 // 	getUserDetails(req, res, function(response) {
